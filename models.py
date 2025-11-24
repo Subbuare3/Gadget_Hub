8019a4cd3ab91aada2731fdad6bb276527841c17
Subbuare3 <n342a664@wichita.edu>
2025-11-23 22:10:27 -0600

    THIS ARE MODELS FOR MY PROJECT

---
+++ models.py
@@ -0,0 +1,14 @@
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(200))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+    stock = db.Column(db.Integer)
